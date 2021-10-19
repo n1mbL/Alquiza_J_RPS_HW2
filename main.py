20cce8760b4ba351ff13921bd48d4826a7abd75c
Jack B <n1mbL@CVN79Kennedy.local>
2021-10-20 00:43:31 +0800

    saving intro Python program

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Spock"
+eyeColor = "black"
+fingers = int(10)
+score = 0
+
+print ("-------------")
+
+print ("My name is " + name + " and I have " + eyeColor + " eyes.")
+print ("I also have " + str(fingers) + " fingers on my two hands.")
